6c93f083d37816373121991458e81bff36e8dff5
hhzz9hhzz9 <34962165+hhzz9hhzz9@users.noreply.github.com>
2020-03-23 18:01:37 +0800

    测试代码2

---
+++ src/main/java/com/woniu/test/App.java
@@ -0,0 +1,11 @@
+package com.woniu.test;
+
+/**
+ * @author llf
+ * @date 2020/3/23 14:32
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("1");
+    }
+}
